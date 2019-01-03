19b3145f19d4071446709ec935917b158db653bf
jon <jon@pixnet.tw>
2019-01-03 09:09:27 +0800

    Initial Commit

---
+++ Homework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homework
+//
+//  Created by jon on 2019/1/3.
+//  Copyright © 2019 jon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Homework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homework
+//
+//  Created by jon on 2019/1/3.
+//  Copyright © 2019 jon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
